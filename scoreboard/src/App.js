d6d346025752e3f5bc152199a54b08208254a490
May Ng <mngmay@gmail.com>
2019-08-08 12:31:57 -0700

    Created component and tests folder/files. Added dashboard/display components to App

---
+++ scoreboard/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Dashboard } from "./components/Dashboard";
+import { Display } from "./components/Display";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+      <Display />
+    </div>
+  );
+}
+
+export default App;
